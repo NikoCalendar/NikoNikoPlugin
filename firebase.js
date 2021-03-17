b3e3e2cf91c18decd498bc81538d0af43832eb3c
Miguel <junior7395@hotmail.es>
2021-03-17 17:48:23 +0100

    Configuración básica para Firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCmtwXWT2Ot1PSDYssOqxZUaT-Cpk0GKh4",
+    authDomain: "nikocalendar-14ee2.firebaseapp.com",
+    projectId: "nikocalendar-14ee2",
+    storageBucket: "nikocalendar-14ee2.appspot.com",
+    messagingSenderId: "217161690206",
+    appId: "1:217161690206:web:4bba17150d08a8f27018cf",
+    measurementId: "G-ZLEN5YL2WP"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  // Get a reference to the database service
+  var database = firebase.database();
